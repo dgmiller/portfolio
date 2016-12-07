0354bad454d90f05a183dbd2a127b1f51938af5c
derekgm <derekgm@byu.local@acme30.byu.edu>
2016-12-06 18:34:43 -0700

    added capstone final project which is a work in progress. Need to update git ignore to exclude data files

---
+++ capstone/script.js
@@ -0,0 +1,21 @@
+var margin = {top: 80, right: 0, bottom: 10, left: 80},
+    width = 720,
+    height = 720;
+
+var x = d3.scale.ordinal().rangeBands([0,width]),
+    z = d3.scale.linear().domain([0,4]).clamp(true),
+    c = d3.scale.category10().domain(d3.range(10));
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .style("margin-left", -margin.left + "px")
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("network.json", function(network) {
+	var matrix = [],
+	    nodes = network.nodes,
+	    n = nodes.length;
+
+
